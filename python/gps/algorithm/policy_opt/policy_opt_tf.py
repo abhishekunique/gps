e44a54a813f97ae2b878fb2cb533ba7a6e38fb10
Abhishek Gupta <abhishekunique@gmail.com>
2016-05-08 15:00:13 -0700

    fixed policy scale bias issues

--- python/gps/algorithm/policy_opt/policy_opt_tf.py
+++ python/gps/algorithm/policy_opt/policy_opt_tf.py
@@ -44,10 +44,6 @@ class PolicyOptTf(PolicyOpt):
             self.action_tensors.append(None)
             self.var.append(self._hyperparams['init_var'] * np.ones(dU_ind))
         self.init_network()
-        # tv = tf.trainable_variables()
-        # num_weights = 10
-        # self.shared_vars_array = tv[:6]
-        # self.other_vars_array = tv[6:]
         self.init_solver()
         self.sess = tf.Session()
         self.policy = []
@@ -76,26 +72,8 @@ class PolicyOptTf(PolicyOpt):
             self.ent_reg = [self._hyperparams['ent_reg']]*self.num_robots
         else:
             self.ent_reg = self._hyperparams['ent_reg']
-        # allVars = tf.trainable_variables()
-        # self.shared_vars = []
-        # for var in allVars:
-        #     if 'shared' in var.name:
-        #         self.shared_vars.append(var)
         init_op = tf.initialize_all_variables()
         self.sess.run(init_op)
-
-        # if 'restore_all_wts' in self._hyperparams and self._hyperparams['restore_all_wts']:
-        #     val_vars = np.load(self._hyperparams['restore_all_wts'])
-        #     for i in range(num_weights):
-        #         assign_op = tv[i].assign(val_vars[i])
-        #         self.sess.run(assign_op)
-        #     import IPython
-        #     IPython.embed()
-
-        # if self._hyperparams['restore_shared_wts']:
-        #     self.restore_shared_wts()
-        # merged = tf.merge_all_summaries()
-        # writer = tf.train.SummaryWriter('/home/abhigupta/tensorboard_data', graph_def=self.sess.graph)
 
     def init_network(self):
         """ Helper method to initialize the tf networks used """
@@ -121,25 +99,9 @@ class PolicyOptTf(PolicyOpt):
         self.combined_loss = tf.add_n(self.loss_scalars)
         self.av = av
         self.ls = ls
-        # self.indiv_vars = indiv_vars
 
     def init_solver(self):
         """ Helper method to initialize the solver. """
-        # self.solvers = []
-        # for robot_number in range(self.num_robots):
-        #     loss = self.loss_scalars[robot_number]
-        #     vars_to_opt = self.indiv_vars[robot_number]
-
-        #     self.solvers.append(TfSolver(loss_scalar=loss,
-        #                                  solver_name=self._hyperparams['solver_type'],
-        #                                  base_lr=self._hyperparams['lr'],
-        #                                  lr_policy=self._hyperparams['lr_policy'],
-        #                                  momentum=self._hyperparams['momentum'],
-        #                                  weight_decay=self._hyperparams['weight_decay'],
-        #                                  # fc_vars=self.fc_vars,
-        #                                  # last_conv_vars=self.last_conv_vars)#,
-        #                                  vars_to_opt=vars_to_opt))
-
         self.solver =TfSolver(loss_scalar=self.combined_loss,
                               solver_name=self._hyperparams['solver_type'],
                               base_lr=self._hyperparams['lr'],
@@ -207,8 +169,8 @@ class PolicyOptTf(PolicyOpt):
             if itr == 0 and inner_itr == 1:
                 #TODO: may need to change this
                 self.policy[robot_number].x_idx = self.x_idx[robot_number]
-                self.policy[robot_number].scale = np.diag(1.0 / np.std(obs[:, self.x_idx[robot_number]], axis=0))
-                self.policy[robot_number].bias = -np.mean(obs[:, self.x_idx[robot_number]].dot(self.policy[robot_number].scale), axis=0)
+                self.policy[robot_number].scale = np.eye(np.diag(1.0 / (np.std(obs[:, self.x_idx[robot_number]], axis=0) + 1e-8)).shape[0])
+                self.policy[robot_number].bias = np.zeros((-np.mean(obs[:, self.x_idx[robot_number]].dot(self.policy[robot_number].scale), axis=0)).shape)
             obs[:, self.x_idx[robot_number]] = obs[:, self.x_idx[robot_number]].dot(self.policy[robot_number].scale) + self.policy[robot_number].bias
 
             # Assuming that N*T >= self.batch_size.
@@ -228,34 +190,6 @@ class PolicyOptTf(PolicyOpt):
             tgt_prc_orig_reshaped.append(tgt_prc_orig)
 
         average_loss = 0
-        print "itr_full", itr_full
-        print "inner_itr", inner_itr
-        if itr_full[0] > 0:
-            feed_dict = {}
-            for robot_number in range(self.num_robots):
-                feed_dict[self.obs_tensors[robot_number]] = obs_reshaped[robot_number]
-
-            num_values = obs_reshaped[0].shape[0]
-            conv_values = self.solver.get_last_conv_values(self.sess, feed_dict, num_values, 100)
-            for i in range(self._hyperparams['fc_only_iterations'] ):
-                feed_dict = {}
-                for robot_number in range(self.num_robots):
-                    start_idx = int(i * self.batch_size %
-                                    (batches_per_epoch_reshaped[robot_number] * self.batch_size))
-                    idx_i = idx_reshaped[robot_number][start_idx:start_idx+self.batch_size]
-                    feed_dict[self.action_tensors[robot_number]] = tgt_mu_reshaped[robot_number][idx_i]
-                    feed_dict[self.precision_tensors[robot_number]] = tgt_prc_reshaped[robot_number][idx_i]
-                    feed_dict[self.last_conv_vars[robot_number]] = conv_values[robot_number][idx_i]
-
-                train_loss = self.solver(feed_dict, self.sess, device_string=self.device_string, use_fc_solver=True)
-                average_loss += train_loss
-                if i % 100 == 0 and i != 0:
-                    LOGGER.debug('tensorflow iteration %d, average loss %f',
-                                 i, average_loss / 100)
-                    print 'supervised fc_only tf loss is '
-                    print (average_loss/100)
-                    average_loss = 0
-        average_loss = 0
         for i in range(self._hyperparams['iterations']):
             # Load in data for this batch.
             feed_dict = {}
@@ -266,8 +200,6 @@ class PolicyOptTf(PolicyOpt):
                 feed_dict[self.obs_tensors[robot_number]] = obs_reshaped[robot_number][idx_i]
                 feed_dict[self.action_tensors[robot_number]] = tgt_mu_reshaped[robot_number][idx_i]
                 feed_dict[self.precision_tensors[robot_number]] = tgt_prc_reshaped[robot_number][idx_i]
-            # if i % 100 == 0:
-            #     print(ffpvals)
             train_loss = self.solver(feed_dict, self.sess, device_string=self.device_string)
 
             average_loss += train_loss
@@ -278,15 +210,8 @@ class PolicyOptTf(PolicyOpt):
                 print (average_loss/100)
                 average_loss = 0
 
-        obs_plot = obs_reshaped[0][1]
-        img_plot = self.single_img_with_fp(obs_plot, 0, 64,80,3)
-        import matplotlib.image as mpimg
-        import time
-        mpimg.imsave('test_itr'+str(time.clock())+'.png', img_plot) 
-
         for robot_number in range(self.num_robots):
             # Keep track of tensorflow iterations for loading solver states.
-            #TODO: Need to figure this out, not going to work
             self.tf_iter[robot_number] += self._hyperparams['iterations']
 
             # Optimize variance.
@@ -330,24 +255,6 @@ class PolicyOptTf(PolicyOpt):
         pol_det_sigma = np.tile(np.prod(self.var[robot_number]), [N, T])
 
         return output, pol_sigma, pol_prec, pol_det_sigma
-
-    def single_img_with_fp(self, obs, robot_number, img_height, img_width, num_channels ):
-        fps = self.feature_points[robot_number]
-        feed_dict = {}
-        batch_obs = np.array([obs])
-        feed_dict[self.obs_tensors[robot_number]] = batch_obs
-        fps_vals= self.sess.run(fps, feed_dict)[0]
-        fx_v = fps_vals[::2].astype(int)
-        fy_v = fps_vals[1::2].astype(int)
-        img = obs[self.img_idx[robot_number]]
-
-        img = img.reshape((num_channels, img_width, img_height))
-        img = np.transpose(img, [2,1,0])
-        img = 255-img
-        img[fx_v, fy_v, :] = 0
-        img[fx_v, fy_v, 0] = 255
-        return img
-
 
     def save_shared_wts(self):
         var_dict = {}
