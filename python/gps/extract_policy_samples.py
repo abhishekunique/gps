67bfccf1acfe82017565a27232f742a6ad172399
Abhishek Gupta <abhishekunique@gmail.com>
2016-06-13 12:50:01 -0700

    picking back up on this codebase

--- python/gps/extract_policy_samples.py
+++ python/gps/extract_policy_samples.py
@@ -40,57 +40,57 @@ offsets = [
     [np.array([0.75, 0.0, -0.55]),np.array([0.45, 0.0, -0.55])],
 ]
 
-# if __name__ == "__main__":
-#     data_logger = DataLogger()
-#     data_dir = '/home/abhigupta/gps/experiments/mjc_multirobot_reach_images_multicondition_conv2shared/data_files/'
-#     onlyfiles = []
-#     for itr in range(8):
-#         onlyfiles.append('pol_sample_itr_%02d_rn_01.pkl' % itr)
-#         targets = all_offsets
-#         dists = []
-#     print onlyfiles
-#     print "------------"
-#     for f in onlyfiles:
-#         traj_sample_list = data_logger.unpickle(data_dir + f)
-#         print data_dir + f
-#         dist_list = [0]*len(traj_sample_list)
-#         for i, cond_list in enumerate(traj_sample_list):
-#             dist = 0
-#             for sample in cond_list._samples:
-#                 sample_ee = sample._data[END_EFFECTOR_POINTS][-20:]
-#                 dist += np.mean(np.linalg.norm(sample_ee - targets[i]-np.array([0.8, 0.0, 0.5]), axis=1))
-#             dist_mean = dist/float(len(cond_list._samples))
-#             dist_list[i] = dist_mean
-#         dists.append(dist_list)
-#     cond_dists = np.asarray(dists)
-#     np.save(data_dir+"mjc_multirobot_reach_conv2shared_r1", cond_dists)
-
 if __name__ == "__main__":
     data_logger = DataLogger()
-    data_dir = '/home/abhigupta/gps/experiments/mjc_singlerobot_3link_push/data_files/'
-    # data_dir = '/home/coline/abhishek_gps/gps/experiments/mjc_multirobot_reach_domain_confusion/data_files/'
+    data_dir = '/home/abhigupta/gps/experiments/fc_4link_push/data_files/'
     onlyfiles = []
-    for itr in range(8):
-        onlyfiles.append('pol_sample_ee_itr_%02d_rn_00.pkl' % itr)
-        targets = [offsets[i][1] for i in range(len(offsets))]
-        print targets
+    for itr in range(0,9):
+        onlyfiles.append('pol_sample_itr_%02d_rn_00.pkl' % itr)
+        targets = all_offsets
         dists = []
+    print onlyfiles
+    print "------------"
     for f in onlyfiles:
         traj_sample_list = data_logger.unpickle(data_dir + f)
         print data_dir + f
         dist_list = [0]*len(traj_sample_list)
         for i, cond_list in enumerate(traj_sample_list):
             dist = 0
-            for sample in cond_list:
-                sample_ee = sample[-20:, 3:]
-                print "ee_shape", sample_ee.shape
+            for sample in cond_list._samples:
+                sample_ee = sample._data[END_EFFECTOR_POINTS][-20:, 3:6]
                 dist += np.mean(np.linalg.norm(sample_ee - targets[i], axis=1))
-            dist_mean = dist/float(len(cond_list))
-            print "mean", dist_mean
+            dist_mean = dist/float(len(cond_list._samples))
             dist_list[i] = dist_mean
-        print dist_list
         dists.append(dist_list)
     cond_dists = np.asarray(dists)
-    np.save(data_dir+"/mjc_singlerobot_3link_push_baseline", cond_dists)
+    np.save(data_dir+"fc_4link_push_trial2", cond_dists)
+
+# if __name__ == "__main__":
+#     data_logger = DataLogger()
+#     data_dir = '/home/abhigupta/gps/experiments/contrastive_fc_push_test/data_files/'
+#     # data_dir = '/home/coline/abhishek_gps/gps/experiments/mjc_multirobot_reach_domain_confusion/data_files/'
+#     onlyfiles = []
+#     for itr in range(8):
+#         onlyfiles.append('pol_sample_ee_itr_%02d_rn_00.pkl' % itr)
+#         targets = [offsets[i][1] for i in range(len(offsets))]
+#         print targets
+#         dists = []
+#     for f in onlyfiles:
+#         traj_sample_list = data_logger.unpickle(data_dir + f)
+#         print data_dir + f
+#         dist_list = [0]*len(traj_sample_list)
+#         for i, cond_list in enumerate(traj_sample_list):
+#             dist = 0
+#             for sample in cond_list:
+#                 sample_ee = sample[-20:, 3:]
+#                 print "ee_shape", sample_ee.shape
+#                 dist += np.mean(np.linalg.norm(sample_ee - targets[i], axis=1))
+#             dist_mean = dist/float(len(cond_list))
+#             print "mean", dist_mean
+#             dist_list[i] = dist_mean
+#         print dist_list
+#         dists.append(dist_list)
+#     cond_dists = np.asarray(dists)
+#     np.save(data_dir+"/mjc_singlerobot_3link_push_baseline", cond_dists)
 
     # np.save("mjc_multirobot_reach_domain_confusion_r1", cond_dists)
