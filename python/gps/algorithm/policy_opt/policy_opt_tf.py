61ee2a148c935ad3d1f7b157030f4aa25c765491
Harley Montgomery <harleymontgomery@gmail.com>
2016-06-23 16:35:08 -0700

    set random seed explicitly in policy_opt_tf

--- python/gps/algorithm/policy_opt/policy_opt_tf.py
+++ python/gps/algorithm/policy_opt/policy_opt_tf.py
@@ -23,6 +23,8 @@ class PolicyOptTf(PolicyOpt):
         self.num_robots = len(dU)
         self.tf_iter = [0 for r_no in range(len(dU))]
         self.checkpoint_prefix = self._hyperparams['checkpoint_prefix']
+        tf.set_random_seed(self._hyperparams['random_seed'])
+
         self.batch_size = self._hyperparams['batch_size']
         self.device_string = "/cpu:0"
         if self._hyperparams['use_gpu'] == 1:
