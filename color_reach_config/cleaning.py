119003d496e5d13604a791cc245604c5e4c95019
Kavi Gupta <kavi@berkeley.edu>
2017-07-03 14:11:49 -0700

    Fixed typo

--- color_reach_config/cleaning.py
+++ color_reach_config/cleaning.py
@@ -4,5 +4,5 @@ USE_IMAGES = False
 ARMS_3D = False
 COLOR_BLOCKS_3D = False
 ROBOT_TYPES = RobotType.THREE_LINK, RobotType.THREE_LINK_SHORT_JOINT, RobotType.FOUR_LINK
-TASK_TYPES = Cleaning, ColoReach("red"), ColorReach("green"), ColorReach("yellow")
+TASK_TYPES = Cleaning, ColorReach("red"), ColorReach("green"), ColorReach("yellow")
 VIDEO_PATH = None
