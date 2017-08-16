d3011970be3d206545d11cfa12d6597198947ba7
Kavi Gupta <kavi@berkeley.edu>
2017-08-15 18:04:50 -0700

    Add color reach configurations.

--- color_reach_config/baxter_images.py
+++ color_reach_config/baxter_images.py
@@ -1,8 +1,8 @@
 SHOW_VIEWER = True
-MODE = "testing"
+MODE = "training"
 ARMS_3D = False
 USE_IMAGES = True
-ROBOT_TYPES = RobotType.BAXTER_CYAN, RobotType.PR2_MAGENTA, RobotType.PEGGY
+ROBOT_TYPES = RobotType.BAXTER, RobotType.PEGGY, RobotType.PR2
 TASK_TYPES = REACHERS
 VIDEO_PATH = None
 
