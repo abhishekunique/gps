f972f5f12b30c097ffe77ecc24d5e6051cb58fda
Kavi Gupta <kavi@berkeley.edu>
2017-08-17 23:18:14 -0700

    Fix real robots experiment

--- color_reach_config/real_robots.py
+++ color_reach_config/real_robots.py
@@ -3,11 +3,10 @@ MODE = "training"
 ARMS_3D = False
 USE_IMAGES = True
 ROBOT_TYPES = (RobotType.PEGGY, False), (RobotType.PR2, True)
-ROBOT_TYPES = reversed(ROBOT_TYPES)
-TASK_TYPES = LegoReach("red"), LegoReach("yellow")
+TASK_TYPES = LegoReach("black"), LegoReach("yellow"), LegoReach("red")
 VIDEO_PATH = None
 
-SAMPLES = 2
+SAMPLES = 4
 
 NEURAL_NET_ITERATIONS *= 0
 NEURAL_NET_ITERATIONS += 2 * 10 ** 5
