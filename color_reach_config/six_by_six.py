19f16fb7ae00be751e7f42222fb75c9c0fd9573e
Kavi Gupta <kavi@berkeley.edu>
2017-09-22 12:43:13 -0700

    Update six by six

--- color_reach_config/six_by_six.py
+++ color_reach_config/six_by_six.py
@@ -1,9 +1,11 @@
-SHOW_VIEWER = True
-MODE = "check-model"
+SHOW_VIEWER = False
+MODE = "check-traj"
 ARMS_3D = False
-USE_IMAGES = True
-ROBOT_TYPES = (RobotType.THREE_LINK, False), (RobotType.THREE_LINK_SHORT_JOINT, False)
+USE_IMAGES = False
+ROBOT_TYPES = (RobotType.PR2, False), (RobotType.BAXTER, False), (RobotType.PEGGY, False), (RobotType.THREE_LINK, False), (RobotType.FOUR_LINK, False), (RobotType.FIVE_LINK, False)
 TASK_TYPES = [ColorReach(color, 6) for color in ("red", "green", "yellow", "black", "magenta", "cyan")]
 VIDEO_PATH = None
 
-BLOCK_LOCATIONS = [np.array([np.cos(theta), 0, np.sin(theta)]) * 0.5 for theta in np.linspace(0, 4, 6)]
+BLOCK_LOCATIONS = [np.array([np.cos(theta), 0, np.sin(theta)]) * 0.75 for theta in np.linspace(0, 4, 6)]
+
+NAME = "six_by_six_try_2"
