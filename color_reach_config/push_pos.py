33248da4ad90bab15015975c51c799d511e1367f
Kavi Gupta <kavi@berkeley.edu>
2017-09-27 21:01:08 -0700

    Renamed and updated new_blockpush -> push_pos

--- color_reach_config/push_pos.py
+++ color_reach_config/push_pos.py
@@ -1,10 +1,11 @@
 SHOW_VIEWER = False
-MODE = "check-all-traj"
+MODE = os.environ['MODE']
 ARMS_3D = False
 USE_IMAGES = False
 import os
 inner_radius = float(os.environ['INNER_RADIUS'])
 diff_radius = float(os.environ['DIFF_RADIUS'])
+z_location = float(os.environ['Z_LOCATION'])
 robot_type = eval(os.environ['ROBOT_TYPE'])
 ROBOT_TYPES = (robot_type, False),
 TASK_TYPES = [BlockPush]
@@ -12,13 +13,15 @@ VIDEO_PATH = None #"/home/kavi/Videos/pos%s" % inner_radius
 
 BLOCKPUSH_ANGLES = [[theta, theta + d_theta] for theta in np.linspace(-2, 2, 6) for d_theta in [-0.4, 0, 0.4]]
 
-def to_cartesian(r, theta):
-    return np.array([np.cos(theta), 0, np.sin(theta)]) * r
+def to_cartesian(r, theta, z=0):
+    return np.array([np.cos(theta), z, np.sin(theta)]) * r
 
-BLOCK_START = [to_cartesian(inner_radius, th) for th, _ in BLOCKPUSH_ANGLES]
+BLOCK_START = [to_cartesian(inner_radius, th, z=-z_location) for th, _ in BLOCKPUSH_ANGLES]
 VELOCITIES  = [to_cartesian(diff_radius, th) for _, th in BLOCKPUSH_ANGLES]
 BLOCKPUSH_BLOCK_LOCATIONS = [[x, x + v] for x, v in zip(BLOCK_START, VELOCITIES)]
 
 
 
-NAME = "blockpush_pos_below_%s_%s_%s" % (inner_radius, diff_radius, os.environ['ROBOT_TYPE'])
+NAME = "push_pos_%s_%s_%s_%s" % (inner_radius, diff_radius, z_location, os.environ['ROBOT_TYPE'])
+
+# VIDEO_PATH =  "/home/kavi/Videos/%s" % NAME
