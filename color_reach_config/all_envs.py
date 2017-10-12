3cb2f68b612993f662d732f16e74d327fdd59917
Coline Devin <coline@berkeley.edu>
2017-10-12 14:05:43 -0700

    Added grid

--- color_reach_config/all_envs.py
+++ color_reach_config/all_envs.py
@@ -26,6 +26,15 @@ VIDEO_PATH = None # "/home/kavi/Videos/pr2vel"
 #ROBOT_TYPES += [(robot_type, False) for robot_type in [RobotType.PR2, RobotType.PEGGY, RobotType.THREE_DF_BLOCK, RobotType.FOUR_SEVEN]]
 TASK_TYPES += [BlockVelocityPush(color, INITIAL_ANGLES, DIFF_ANGLES, INNER_RADIUS, DIFF_RADIUS) for color in "red", "yellow", "green"]
 
+INNER_RADIUS = 0.75
+ANGLES = [-2.2, -1.8, -1.2, 1.2, 1.8, 2.2]
+TASK_TYPES += [ColorReachRYG(color, ANGLES, INNER_RADIUS) for color in "red", "yellow", "green"]
+VIDEO_PATH = None #"/home/kavi/Videos/pos%s" % inner_radius
+
+NAME = "color_reach_ryg%s" % stem
+
+if MODE == "check-all-traj":
+    VIDEO_PATH =  "/home/kavi/Videos/%s" % NAME
 
 if MODE == "check-all-traj":
     VIDEO_PATH = "/home/kavi/Videos/0930-%s" % NAME
--- experiments/color_reach/hyperparams.py
+++ experiments/color_reach/hyperparams.py
@@ -121,19 +121,24 @@ for robot_n, robot_type in enumerate(ROBOT_TYPES):
         arguments.append((task_type, robot_type))
 
 leave_one_out = LEAVE_ONE_OUT
-grid1_training = [6,7,8,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
-grid1_testing = [0,1,2,3,4,5,9,10,11,18,19,20]
+GRID  =  os.environ['GRID']  if 'GRID' in os.environ else 0
+if GRID == 0:
+    grid_training = range(len(arguments))
+    grid_testing = range(len(arguments))
+elif GRID == 1:
+    grid_training = [6,7,8,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+    grid_testing = [0,1,2,3,4,5,9,10,11,18,19,20]
 
 if MODE == "training-trajectories" or MODE == "check-all-traj":
     task_values, robot_values, arguments = zip(*((task, robot, arg) for task, robot, arg in zip(task_values, robot_values, arguments) if not arg[1][1]))
 elif IS_TESTING:
-    task_values     = [task_values[i] for i in grid1_testing]
-    robot_values    = [robot_values[i] for i in grid1_testing]
-    arguments       = [arguments[i] for i in grid1_testing]
+    task_values     = [task_values[i] for i in grid_testing]
+    robot_values    = [robot_values[i] for i in grid_testing]
+    arguments       = [arguments[i] for i in grid_testing]
 else:
-    task_values     = [task_values[i] for i in grid1_training]
-    robot_values    = [robot_values[i] for i in grid1_training]
-    arguments       = [arguments[i] for i in grid1_training]
+    task_values     = [task_values[i] for i in grid_training]
+    robot_values    = [robot_values[i] for i in grid_training]
+    arguments       = [arguments[i] for i in grid_training]
 
 agents = [reacher_by_color_and_type(i,
                                     len(arguments),
